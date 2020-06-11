57f1fef063434a5bacc73d024fadf4d212fbc364
m-nito <masamisce@gmail.com>
2020-06-11 22:03:05 +0900

    Add comments on eventMessages

--- src/eventMessages.ts
+++ src/eventMessages.ts
@@ -1,5 +1,24 @@
+/**
+ * Fired when app-ready.
+ */
 export const EVT_APP_READY = "ready";
+
+/**
+ * Fired when app-close.
+ */
 export const EVT_APP_CLOSE = "close";
+
+/**
+ * Fired when user is tring to close app.
+ */
 export const EVT_CLOSING = "closing";
-export const EVT_SRC_SELECTED = "EVT_SRC_SELECTED";
+
+/**
+ * Fired when app tries to show source selector.
+ */
 export const EVT_SHOW_SELECTOR = "showSourceSelector";
+
+/**
+ * Fired when user decided video source.
+ */
+export const EVT_SRC_SELECTED = "EVT_SRC_SELECTED";
